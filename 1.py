d9d0086f10843de49e79e816733b7d3805646e35
Research-167-059-Deployment <research2020e059e167engjfn@gmail.com>
2024-10-31 09:46:20 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,7 @@
+echo "# power-consumption-predictor" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Research-167-059-Deployment/power-consumption-predictor.git
+git push -u origin main
